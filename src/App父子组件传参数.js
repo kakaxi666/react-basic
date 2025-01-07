9c4893181bf0bb6f96c1b6bee30785b1e42d79dc
kakaxi666 <an456789123@163.com>
2025-01-07 11:27:07 +0800

    父子组件传参数

---
+++ src/App父子组件传参数.js
@@ -0,0 +1,28 @@
+function Son(props){
+    // return <div>我是子组件{props.name}{props.child}</div>
+    console.log(props);
+    return <div>我是子组件{props.children}</div>
+}
+
+function App() {
+    const name = '张三'
+  return (
+    <div>
+        {/* <Son 
+            name={name}
+            age={18}
+            isTrues={true}
+            list={[1,2,3]}
+            obj={{name:'李四'}}
+            cb={()=>console.log('回调函数')}
+            child={<span>this is span</span>}
+        /> */}
+        <Son>
+            <span>this is span</span>
+        </Son>
+    </div>
+  )
+    
+}
+
+export default App;
